71ee60daacf666bbb4b2fab68160f0eb1a82e754
lllisteu <lllisteu@users.noreply.github.com>
2020-11-28 10:32:06 +0100

    Refine Device#identify.

--- lib/tinderfridge/device.rb
+++ lib/tinderfridge/device.rb
@@ -5,9 +5,10 @@ module Tinkerforge
 
     # Identifies a Brick, Bricklet, or other Tinkerforge component by blinking its status led.
     def identify(seconds=10)
+      seconds = seconds.to_i
       if (respond_to? 'get_status_led_config') and (respond_to? 'set_status_led_config')
         state = get_status_led_config
-        (seconds.to_i*2).times do |n|
+        (seconds*2).times do |n|
           set_status_led_config n.remainder(2)
           sleep 0.5
         end
