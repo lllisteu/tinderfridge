6bc9684449365a35b80bac36e6c5bd038998b1fa
lllisteu <lllisteu@users.noreply.github.com>
2020-11-30 13:32:34 +0100

    Add Device#inspect.

--- lib/tinderfridge/device.rb
+++ lib/tinderfridge/device.rb
@@ -3,6 +3,15 @@ require 'tinkerforge/ip_connection'
 module Tinkerforge
   class Device
 
+    # Returns a programmer-friendly representation of the device.
+    def inspect
+      if (instance_variable_defined? :@ipcon) and (instance_variable_defined? :@uid_string)
+        "%s (%s@%s:%s)" % [self.class, @uid_string, @ipcon.host, @ipcon.port]
+      else
+        super
+      end
+    end
+
     # Identifies a Brick, Bricklet, or other Tinkerforge component by blinking its status led.
     def identify(seconds=10)
       seconds = seconds.to_i
