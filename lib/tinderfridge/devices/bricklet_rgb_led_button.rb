f316bbbc58393b8b60b98e94eeb1bf685d50f9e0
lllisteu <lllisteu@users.noreply.github.com>
2020-12-10 09:42:06 +0100

    Add BrickletRGBLEDButton#rgb & #rgb= methods.

---
+++ lib/tinderfridge/devices/bricklet_rgb_led_button.rb
@@ -0,0 +1,19 @@
+module Tinkerforge
+
+  class BrickletRGBLEDButton
+
+    # Returns the button's current color as three values for red, green, and blue (integers in the range 0..255).
+    def rgb
+      get_color
+    end
+
+    # Sets the button's color using three values for red, green, and blue (integers in the range 0..255).
+    #
+    # Values can be supplied as an array or as three separate values.
+    def rgb=(*rgb)
+      set_color *rgb.flatten
+    end
+
+  end
+
+end
