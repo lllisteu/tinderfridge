aac4a4263946a277a9684a0410e31afce4648da0
lllisteu <lllisteu@users.noreply.github.com>
2020-12-11 08:44:19 +0100

    Add BrickletLEDStripV2#blackout method.

---
+++ lib/tinderfridge/devices/bricklet_led_strip_v2.rb
@@ -0,0 +1,15 @@
+module Tinkerforge
+
+  class BrickletLEDStripV2
+
+    # Switches off all connected LEDs. Use in case of misconfiguration or other trouble.
+    #
+    # Sends the connected LEDs 6144 zeros (2048 RGB values or 1536 RGBW values).
+    def blackout
+      a = Array.new(1024,0)
+      6.times { |n| set_led_values( n*1024, a ) }
+    end
+
+  end
+
+end
