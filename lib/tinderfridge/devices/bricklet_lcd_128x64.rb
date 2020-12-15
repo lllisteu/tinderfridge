b24d6e1b071be65cd885a7ae0270669148ff6bc1
lllisteu <lllisteu@users.noreply.github.com>
2020-12-15 14:37:11 +0100

    Add BrickletLCD128x64#backlight, #backlight=, #clear methods.

---
+++ lib/tinderfridge/devices/bricklet_lcd_128x64.rb
@@ -0,0 +1,25 @@
+module Tinkerforge
+
+  class BrickletLCD128x64
+
+    # Returns the current setting for the backlight (0..100).
+    def backlight
+      get_display_configuration[1]
+    end
+
+    # Sets the brightness of the backlight (0..100).
+    def backlight=(brightness)
+      state = get_display_configuration
+      state[1] = brightness.to_i
+      set_display_configuration *state
+    end
+
+    # Clears the display, including the GUI.
+    def clear
+      clear_display
+      remove_all_gui
+    end
+
+  end
+
+end
