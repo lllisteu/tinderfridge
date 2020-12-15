9afef1db4f71f6c6b76f36527c79b1e098b1188c
lllisteu <lllisteu@users.noreply.github.com>
2020-12-15 14:35:52 +0100

    Add BrickletGPSV2#coordinates method.

--- lib/tinderfridge/devices/bricklet_gps_v2.rb
+++ lib/tinderfridge/devices/bricklet_gps_v2.rb
@@ -2,6 +2,21 @@ module Tinkerforge
 
   class BrickletGPSV2
 
+    # Returns latitude and longitude as reported by the GPS Bricklet.
+    #
+    # Nil when there is no fix (position not determined).
+    def coordinates
+      if get_status[0]
+        c = get_coordinates
+        [
+          c[0] / (c[1] == 'N' ? 1000000.0 : -1000000.0),
+          c[2] / (c[3] == 'E' ? 1000000.0 : -1000000.0)
+        ]
+      else
+        nil
+      end
+    end
+
     # Returns a Time object representing the time as reported by the GPS Bricklet.
     def time
       # FIXME: This will not work after 31-Dec-2099.
