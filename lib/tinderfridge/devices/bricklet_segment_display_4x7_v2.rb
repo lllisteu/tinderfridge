a983eef5d49eaa0eaeacd20b7a961997c3e56bbb
lllisteu <lllisteu@users.noreply.github.com>
2021-01-26 15:47:55 +0100

    Add BrickletSegmentDisplay4x7V2#segments= method.

--- lib/tinderfridge/devices/bricklet_segment_display_4x7_v2.rb
+++ lib/tinderfridge/devices/bricklet_segment_display_4x7_v2.rb
@@ -15,6 +15,12 @@ module Tinkerforge
       send_request FUNCTION_GET_SEGMENTS, [], '', 14, '?35'
     end
 
+    # Sets the state of all 35 segments.
+    def segments=(*segments)
+      segs = segments.flatten
+      set_segments segs[0,8], segs[8,8], segs[16,8], segs[24,8], segs[32,2], segs[34]
+    end
+
   end
 
 end
