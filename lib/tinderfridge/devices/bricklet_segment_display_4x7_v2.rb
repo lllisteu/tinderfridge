5542845e4641a7dc6c03347e914b3f93d1adec37
lllisteu <lllisteu@users.noreply.github.com>
2021-01-27 09:30:39 +0100

    Add BrickletSegmentDisplay4x7V2#segments_string= method.

--- lib/tinderfridge/devices/bricklet_segment_display_4x7_v2.rb
+++ lib/tinderfridge/devices/bricklet_segment_display_4x7_v2.rb
@@ -15,15 +15,24 @@ module Tinkerforge
       send_request FUNCTION_GET_SEGMENTS, [], '', 14, '?35'
     end
 
+    # Sets the state of all 35 segments.
+    def segments=(*segments)
+      segs = segments.flatten
+      set_segments segs[0,8], segs[8,8], segs[16,8], segs[24,8], segs[32,2], segs[34]
+    end
+
     # Returns the state of all 35 segments as a string of 1s and 0s.
     def segments_string
       segments.map { |s| s ? '1' : '0' }.join
     end
 
-    # Sets the state of all 35 segments.
-    def segments=(*segments)
-      segs = segments.flatten
-      set_segments segs[0,8], segs[8,8], segs[16,8], segs[24,8], segs[32,2], segs[34]
+    # Sets the state of all 35 segments using a string of 1s and 0s.
+    #
+    # For readability, the string may include spaces and underscores, which will be ignored.
+    # @example
+    #  my_bricklet.segments_string = '10000110 11000110 11000110 00000110 00 0'
+    def segments_string=(segments)
+      self.segments = segments.gsub(/[ _]/, '').ljust(35, '0').chars.map { |s| s == '1' }
     end
 
   end
