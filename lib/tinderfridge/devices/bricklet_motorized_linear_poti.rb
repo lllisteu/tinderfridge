bba7c78e47925ef33f033fb53657151efe8fd61d
lllisteu <lllisteu@users.noreply.github.com>
2021-03-02 09:39:12 +0100

    Add BrickletMotorizedLinearPoti#position, #position=, #move_to.

--- lib/tinderfridge/devices/bricklet_motorized_linear_poti.rb
+++ lib/tinderfridge/devices/bricklet_motorized_linear_poti.rb
@@ -2,6 +2,21 @@ module Tinkerforge
 
   class BrickletMotorizedLinearPoti
 
+    # Returns the position of the potentiometer (an integer in the range 0..100).
+    def position
+      get_position
+    end
+
+    # Moves the potentiometer to the given position.
+    #
+    # If the potentiometer was locked before, it will be locked at the new position.
+    # The drive mode (speed) used will be as specified by the last used set_motor_position call.
+    def position=(position)
+      set_motor_position( position, *get_motor_position[1,2] )
+    end
+
+    alias move_to position=
+
     # Retains the position of the potentiometer. If a position is given, moves to that position first.
     def lock(position=nil)
       set_motor_position( position || get_position, get_motor_position[1], true )
