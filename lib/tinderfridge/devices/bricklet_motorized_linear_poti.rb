3e71d447cc3ef77615f8a2363cdc128e1fdee7be
lllisteu <lllisteu@users.noreply.github.com>
2021-03-02 09:37:52 +0100

    Add BrickletMotorizedLinearPoti#lock, #hold, #release.

--- lib/tinderfridge/devices/bricklet_motorized_linear_poti.rb
+++ lib/tinderfridge/devices/bricklet_motorized_linear_poti.rb
@@ -2,6 +2,18 @@ module Tinkerforge
 
   class BrickletMotorizedLinearPoti
 
+    # Retains the position of the potentiometer. If a position is given, moves to that position first.
+    def lock(position=nil)
+      set_motor_position( position || get_position, get_motor_position[1], true )
+    end
+
+    alias hold lock
+
+    # Releases the potentiometer, so its position can be changed by hand.
+    def release
+      set_motor_position( get_position, get_motor_position[1], false )
+    end
+
     # Returns the device's state.
     def state
       super.merge(
