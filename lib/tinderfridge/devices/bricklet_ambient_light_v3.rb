06cf706fbdb5d5430724b5b869093a2f306c3f39
lllisteu <lllisteu@users.noreply.github.com>
2021-03-03 10:33:12 +0100

    Avoid Infinity in constant.

--- lib/tinderfridge/devices/bricklet_ambient_light_v3.rb
+++ lib/tinderfridge/devices/bricklet_ambient_light_v3.rb
@@ -2,13 +2,13 @@ module Tinkerforge
 
   class BrickletAmbientLightV3
 
-    ILLUMINANCE_RANGE = [ 64000, 32000, 16000, 8000, 1300, 600, Float::INFINITY ]
+    ILLUMINANCE_RANGES = [ 64000, 32000, 16000, 8000, 1300, 600, 100000 ]
 
     # Returns the device's state.
     def state
       super.merge(
         'configuration'    => conf = get_configuration,
-        'illuminance_max'  => ILLUMINANCE_RANGE[ conf[0] ],
+        'illuminance_max'  => ILLUMINANCE_RANGES[ conf[0] ],
         'integration_time' => 50 + conf[1] * 50,
         'illuminance_raw'  => get_illuminance,
       )
