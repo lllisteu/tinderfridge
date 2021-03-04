800806db97767e3fe91a0b71897f25d4c0b4553a
lllisteu <lllisteu@users.noreply.github.com>
2021-03-04 13:57:01 +0100

    Add IPConnection#local?

--- lib/tinderfridge/ip_connection.rb
+++ lib/tinderfridge/ip_connection.rb
@@ -15,6 +15,11 @@ module Tinkerforge
       "%s (%s:%s)" % [self.class, host, port]
     end
 
+    # Returns true if using a local connection, false if connected via the network.
+    def local?
+      %w(localhost 127.0.0.1 ::1).include? host
+    end
+
     # Returns a Tinkerforge::DeviceCollection with devices discovered for this IP Connection. Discovery may take a few moments.
     #
     # Accepts an optional argument for the number of seconds to wait, otherwise returns immediately.
