3b74f7912e026a9ed744031fff8b39eb2d648ff2
lllisteu <lllisteu@users.noreply.github.com>
2021-03-10 08:32:30 +0100

    #release takes position arg; comments & docs.

--- lib/tinderfridge/devices/bricklet_motorized_linear_poti.rb
+++ lib/tinderfridge/devices/bricklet_motorized_linear_poti.rb
@@ -10,23 +10,23 @@ module Tinkerforge
     # Moves the potentiometer to the given position.
     #
     # If the potentiometer was locked before, it will be locked at the new position.
-    # The drive mode (speed) used will be as specified by the last used set_motor_position call.
+    # The drive mode (speed) will be the same as the last time the position was set.
     def position=(position)
       set_motor_position( position, *get_motor_position[1,2] )
     end
 
     alias move_to position=
 
-    # Retains the position of the potentiometer. If a position is given, moves to that position first.
+    # Locks the position of the potentiometer. If a position is given, moves to that position first.
     def lock(position=nil)
       set_motor_position( position || get_position, get_motor_position[1], true )
     end
 
     alias hold lock
 
-    # Releases the potentiometer, so its position can be changed by hand.
-    def release
-      set_motor_position( get_position, get_motor_position[1], false )
+    # Releases the potentiometer, so its position can be changed by hand. If a position is given, moves to that position first.
+    def release(position=nil)
+      set_motor_position( position || get_position, get_motor_position[1], false )
     end
 
     # Returns the device's state.
