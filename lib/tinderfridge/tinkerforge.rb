59cb031522d97a36cf034ab7bcb7ad9ae57850ac
lllisteu <lllisteu@users.noreply.github.com>
2021-03-27 10:57:01 +0100

    Add constants for versions URLs.

--- lib/tinderfridge/tinkerforge.rb
+++ lib/tinderfridge/tinkerforge.rb
@@ -2,6 +2,9 @@ require 'tinkerforge/ip_connection'
 
 module Tinkerforge
 
+  ALL_VERSIONS_URL    = 'https://download.tinkerforge.com/all_versions.txt'
+  LATEST_VERSIONS_URL = 'https://download.tinkerforge.com/latest_versions.txt'
+
   # Returns the directory where Tinkerforge bindings appear to be installed.
   def self.lib_dir
     File.dirname File.dirname Device.instance_method('uid').source_location.first
