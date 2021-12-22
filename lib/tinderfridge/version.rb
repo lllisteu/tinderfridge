9abdc99a60a58b39f7f8159f5ce112c9ae4d9eef
lllisteu <lllisteu@users.noreply.github.com>
2021-12-22 17:06:19 +0100

    Bump version to 0.8.2

--- lib/tinderfridge/version.rb
+++ lib/tinderfridge/version.rb
@@ -3,7 +3,7 @@ require 'tinkerforge/version'
 module Tinkerforge
 
   # Tinderfridge version.
-  TINDERFRIDGE_VERSION = '0.8.1'
+  TINDERFRIDGE_VERSION = '0.8.2'
 
   # About Tinkerforge & Tinderfridge.
   def self.about
