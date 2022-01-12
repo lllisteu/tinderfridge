56ddbc6b9956243763ad418b9f90a15cb1fb5cec
lllisteu <lllisteu@users.noreply.github.com>
2022-01-12 10:27:16 +0100

    Add CHIP_TEMPERATURE_UNIT

---
+++ lib/tinderfridge/devices/brick_master/brick_master.rb
@@ -0,0 +1,11 @@
+module Tinkerforge
+
+  class BrickMaster
+
+    # Unit returned by #get_chip_temperature. Master Bricks return temperature as 1/10 °C.
+    CHIP_TEMPERATURE_UNIT = 0.1
+    # REVIEW: This should ideally be part of base Tinkerforge.
+
+  end
+
+end
