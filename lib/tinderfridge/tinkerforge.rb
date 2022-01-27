27c5e15b79991f43bd76e3cc05043487fedead4f
lllisteu <lllisteu@users.noreply.github.com>
2022-01-27 15:54:29 +0100

    Refactor

--- lib/tinderfridge/tinkerforge.rb
+++ lib/tinderfridge/tinkerforge.rb
@@ -5,16 +5,20 @@ module Tinkerforge
   ALL_VERSIONS_URL    = 'https://download.tinkerforge.com/all_versions.txt'
   LATEST_VERSIONS_URL = 'https://download.tinkerforge.com/latest_versions.txt'
 
-  # Returns the directory where Tinkerforge bindings appear to be installed.
-  def self.lib_dir
-    File.dirname File.dirname Device.instance_method('uid').source_location.first
-  end
+  class << self
 
-  # Attempts to load all files that are part of Tinkerforge. Returns a list of files loaded.
-  def self.require_everything
-    Dir.glob(File.join lib_dir, 'tinkerforge', '*.rb').map do |file|
-      require(file) ? file : nil
-    end.compact
+    # Returns the directory where Tinkerforge bindings appear to be installed.
+    def lib_dir
+      File.dirname File.dirname Device.instance_method('uid').source_location.first
+    end
+
+    # Attempts to load all files that are part of Tinkerforge. Returns a list of files loaded.
+    def require_everything
+      Dir.glob(File.join lib_dir, 'tinkerforge', '*.rb').map do |file|
+        require(file) ? file : nil
+      end.compact
+    end
+
   end
 
 end
