0df12a4b6b72bfb24ee82270a4d85bbb109ff90e
lllisteu <lllisteu@users.noreply.github.com>
2022-03-01 14:12:15 +0100

    First stab at #qff

--- lib/tinderfridge/devices/bricklet_air_quality/bricklet_air_quality.rb
+++ lib/tinderfridge/devices/bricklet_air_quality/bricklet_air_quality.rb
@@ -15,6 +15,21 @@ module Tinkerforge
       )
     end
 
+    # Returns the derived air pressure at sea level.
+    #
+    # Air pressure is derived from:
+    # - air pressure as measured by the Bricklet itself
+    # - temperature as measured by the Bricklet itself
+    # - altitude of the Bricklet's location (meter)
+    #
+    # With help from:
+    # - https://www.tinkerforge.com/en/doc/Hardware/Bricklets/Barometer_V2.html
+    # - https://keisan.casio.com/exec/system/1224575267
+    def qff(altitude)
+    # QFE / [1 - Tg * H / (273.15 + Tfe + Tg * H)] ^ (0.034163 / Tg)
+      (get_air_pressure / (1 - 0.0065 * altitude / (273.15 + (get_temperature * 0.01) + 0.0065 * altitude)) ** (0.034163 / 0.0065)).round
+    end
+
   end
 
 end
