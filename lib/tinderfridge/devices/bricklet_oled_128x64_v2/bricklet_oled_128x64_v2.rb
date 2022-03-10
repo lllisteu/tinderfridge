1d8aa64e68fe97086c70ffe11e30911ce1136ca6
lllisteu <lllisteu@users.noreply.github.com>
2022-03-10 07:20:06 +0100

    Refine #encode

--- lib/tinderfridge/devices/bricklet_oled_128x64_v2/bricklet_oled_128x64_v2.rb
+++ lib/tinderfridge/devices/bricklet_oled_128x64_v2/bricklet_oled_128x64_v2.rb
@@ -9,7 +9,27 @@ module Tinkerforge
     # See:
     # - https://en.wikipedia.org/wiki/Code_page_437
     def encode(string)
-      string.encode('IBM437', invalid: :replace, undef: :replace)
+      string.encode('IBM437', fallback: encoding_fallback_table)
+    end
+
+    private
+
+    # Ruby's IBM437 encoding does not include codepoints 0..31 and 127.
+    # This fallback table defines those, minus 0,
+    # and defines '?' as default for undefined characters.
+    def encoding_fallback_table
+      @@encoding_fallback_table ||= Hash.new('?').merge(
+        {
+          '☺'=> "\x01", '☻'=> "\x02", '♥'=> "\x03", '♦'=> "\x04",
+          '♣'=> "\x05", '♠'=> "\x06", '•'=> "\x07", '◘'=> "\x08",
+          '○'=> "\x09", '◙'=> "\x0a", '♂'=> "\x0b", '♀'=> "\x0c",
+          '♪'=> "\x0d", '♫'=> "\x0e", '☼'=> "\x0f", '►'=> "\x10",
+          '◄'=> "\x11", '↕'=> "\x12", '‼'=> "\x13", '¶'=> "\x14",
+          '§'=> "\x15", '▬'=> "\x16", '↨'=> "\x17", '↑'=> "\x18",
+          '↓'=> "\x19", '→'=> "\x1a", '←'=> "\x1b", '∟'=> "\x1c",
+          '↔'=> "\x1d", '▲'=> "\x1e", '▼'=> "\x1f", '⌂'=> "\x7f"
+        }
+      )
     end
 
   end
