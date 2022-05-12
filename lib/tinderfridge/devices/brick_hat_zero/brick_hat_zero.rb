a6e2efb5f534b801895aaf1794a981372e1f63bf
lllisteu <lllisteu@users.noreply.github.com>
2022-05-12 07:08:12 +0200

    Add BrickHATZero 21x8 view

--- lib/tinderfridge/devices/brick_hat_zero/brick_hat_zero.rb
+++ lib/tinderfridge/devices/brick_hat_zero/brick_hat_zero.rb
@@ -9,6 +9,16 @@ module Tinkerforge
       )
     end
 
+    private
+
+    def _view_21x8
+      <<~ET
+      HAT Zero     #{uid_string.rjust 8}
+
+      USB #{'%5.2f V' % [get_usb_voltage*0.001]}
+      ET
+    end
+
   end
 
 end
