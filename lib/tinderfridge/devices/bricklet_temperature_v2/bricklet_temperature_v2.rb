62578bb56b0db3b212c10e70c9daa482ce6e50c5
lllisteu <lllisteu@users.noreply.github.com>
2022-05-12 16:16:07 +0200

    Add BrickletTemperatureV2#state & 21x8 view

---
+++ lib/tinderfridge/devices/bricklet_temperature_v2/bricklet_temperature_v2.rb
@@ -0,0 +1,27 @@
+module Tinkerforge
+
+  class BrickletTemperatureV2
+
+    # Returns the device's state.
+    def state
+      super.merge(
+        'temperature'          => get_temperature,
+        'heater_configuration' => get_heater_configuration,
+      )
+    end
+
+#    private
+
+    def _view_21x8
+      <<~ET
+      Temp         #{uid_string.rjust 8}
+
+            #{'%6.2f °C' % [get_temperature*0.01]}
+
+              #{get_heater_configuration == 1 ? 'HEATER' : ''}
+      ET
+    end
+
+  end
+
+end
