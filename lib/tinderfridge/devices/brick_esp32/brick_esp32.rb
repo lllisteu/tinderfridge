d4b3ca4303ba4aa9b62c739769c4a29b6306f62d
lllisteu <lllisteu@users.noreply.github.com>
2022-05-17 15:48:45 +0200

    ESP32 21x8 views

---
+++ lib/tinderfridge/devices/brick_esp32/brick_esp32.rb
@@ -0,0 +1,13 @@
+module Tinkerforge
+
+  class BrickESP32
+
+    private
+
+    def _view_21x8
+      "ESP32        #{uid_string.rjust 8}"
+    end
+
+  end
+
+end
---
+++ lib/tinderfridge/devices/brick_esp32_ethernet/brick_esp32_ethernet.rb
@@ -0,0 +1,13 @@
+module Tinkerforge
+
+  class BrickESP32Ethernet
+
+    private
+
+    def _view_21x8
+      "ESP32 Eth    #{uid_string.rjust 8}"
+    end
+
+  end
+
+end
