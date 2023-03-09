7a71b9d4775987f8ed4bc2f5fd5a5c596f8a14d1
lllisteu <lllisteu@users.noreply.github.com>
2023-03-09 15:42:40 +0100

    Version 0.14.1

--- lib/tinderfridge/version.rb
+++ lib/tinderfridge/version.rb
@@ -3,7 +3,7 @@ require 'tinkerforge/version'
 module Tinkerforge
 
   # Tinderfridge version.
-  TINDERFRIDGE_VERSION = '0.14.0'
+  TINDERFRIDGE_VERSION = '0.14.1'
 
   # About Tinkerforge & Tinderfridge.
   def self.about
