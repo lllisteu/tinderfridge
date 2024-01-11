8f0af2670a22d1af3471af52d8d44c83a59da791
lllisteu <lllisteu@users.noreply.github.com>
2024-01-11 16:14:29 +0100

    BrickMaster#wifi2_state (draft)

--- lib/tinderfridge/devices/brick_master/brick_master.rb
+++ lib/tinderfridge/devices/brick_master/brick_master.rb
@@ -19,6 +19,33 @@ module Tinkerforge
       ].compact.to_h
     end
 
+    # Returns the state of the WIFI Extension 2.0.
+    def wifi2_state
+      if is_wifi2_present
+        [
+          # Draft generated with:
+          # methods.grep(/^get_wifi2_/).map(&:to_s).map { |m| "          safe_send_state('%s', '%s'),\n" % [m.sub(/^get_wifi2_/, ''), m] }.join
+          safe_send_state('mesh_configuration', 'get_wifi2_mesh_configuration'),
+          safe_send_state('mesh_router_ssid', 'get_wifi2_mesh_router_ssid'),
+          safe_send_state('mesh_router_password', 'get_wifi2_mesh_router_password'),
+          safe_send_state('mesh_common_status', 'get_wifi2_mesh_common_status'),
+          safe_send_state('mesh_client_status', 'get_wifi2_mesh_client_status'),
+          safe_send_state('mesh_ap_status', 'get_wifi2_mesh_ap_status'),
+          safe_send_state('authentication_secret', 'get_wifi2_authentication_secret'),
+          safe_send_state('configuration', 'get_wifi2_configuration'),
+          safe_send_state('status', 'get_wifi2_status'),
+          safe_send_state('client_configuration', 'get_wifi2_client_configuration'),
+          safe_send_state('client_hostname', 'get_wifi2_client_hostname'),
+          safe_send_state('client_password', 'get_wifi2_client_password'),
+          safe_send_state('ap_configuration', 'get_wifi2_ap_configuration'),
+          safe_send_state('ap_password', 'get_wifi2_ap_password'),
+          safe_send_state('firmware_version', 'get_wifi2_firmware_version'),
+        ].compact.to_h
+      end
+    rescue
+        nil
+    end
+
     private
 
     # Safely call a method for inclusion in state hash.
