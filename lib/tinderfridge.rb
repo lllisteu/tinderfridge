59c4905f41afa04c3f5cc9a6219dad23beb9f1b5
lllisteu <lllisteu@users.noreply.github.com>
2024-06-18 09:25:36 +0200

    Refactor (de-obfuscate)

--- lib/tinderfridge.rb
+++ lib/tinderfridge.rb
@@ -2,18 +2,9 @@ if defined? Tinkerforge
   raise 'Tinkerforge was already loaded, so Tinderfridge can not reliably extend it!'
 end
 
-%w(
-
-  version
-
-  ip_connection
-
-  device
-
-  tinkerforge
-
-  device_info
-
-  device_collection
-
-).each { |m| require "tinderfridge/#{m}" }
+require 'tinderfridge/version'
+require 'tinderfridge/ip_connection'
+require 'tinderfridge/device'
+require 'tinderfridge/tinkerforge'
+require 'tinderfridge/device_info'
+require 'tinderfridge/device_collection'
